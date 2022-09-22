0095dcd56666a193d98eae75f1de29523b650aa2
Alexis Torosina <alexistorosina1@gmail.com>
2022-09-22 18:56:11 -0400

    commit tree 9cc0726d6b63f2b39ff0a717f32fd3cc7551429b

---
+++ index.js
@@ -0,0 +1,12 @@
+function divide (num1, num2) {
+    return num1 / num2;
+}
+divide(2000/100);
+
+let square = (x) => {
+    return x * x;
+};
+
+const add = (item, item2) => {
+    return item+item2;
+}
